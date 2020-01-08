625d8b11f67ec128b09eb87f396410a8e570c639
Serafim <nesk@xakep.ru>
2020-01-08 20:16:30 +0300

    Change viewport

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,8 +2,7 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+    <meta name="viewport" content="width=576, user-scalable=no" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
 
     <meta name="csrf-token" content="{{ csrf_token() }}" />
