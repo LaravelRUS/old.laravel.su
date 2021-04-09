7262ed572bc27aecd532788ba1ec65d9e6672b09
slider23 <slider23@gmail.com>
2021-04-09 11:29:19 +0300

    Remove link to repo

--- resources/views/page/docs/partials/translation-notification.blade.php
+++ resources/views/page/docs/partials/translation-notification.blade.php
@@ -1,3 +1,3 @@
 <div class="translation-status">
-    Это скорректированный автоматический перевод, сделанный при помощи google translate: <a href="https://github.com/russsiq/laravel-docs-8.x-ru" target="_blank">https://github.com/russsiq/laravel-docs-8.x-ru</a>
+    Это скорректированный автоматический перевод, сделанный при помощи google translate.
 </div>
