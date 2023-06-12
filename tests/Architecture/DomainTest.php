82d245d402751b9b04318bdc70b3d3bcb6e4488b
Kirill Nesmeyanov <kirill.nesmeianov@tarkov.com>
2023-06-12 03:39:03 +0300

    Add architecture tests

--- tests/Architecture/DomainTest.php
+++ tests/Architecture/DomainTest.php
@@ -1,3 +1,23 @@
 <?php
 
 declare(strict_types=1);
+
+namespace Tests\Architecture;
+
+use PHPat\Selector\Selector;
+use PHPat\Test\Builder\Rule;
+use PHPat\Test\PHPat;
+
+class DomainTest
+{
+    public function testDomainDoesNotDependOnOtherLayers(): Rule
+    {
+        return PHPat::rule()
+            ->classes(Selector::namespace('App\Domain'))
+            ->shouldNotDependOn()
+            ->classes(
+                Selector::namespace('App\Application'),
+                Selector::namespace('App\Infrastructure'),
+            );
+    }
+}
