8a447ed1b7783ad649af5327538b9bb492e44ff6
tabuna <bliz48rus@gmail.com>
2023-11-07 04:55:18 +0300

    Removed pagination (By load always all)

--- resources/views/components/comments.blade.php
+++ resources/views/components/comments.blade.php
@@ -7,37 +7,11 @@
 @endphp
 
 @if($comments->count() < 1)
-    <div class="alert alert-warning">Тут ещё нет комментариев</div>
+    <div class="alert alert-warning p-5">Тут ещё нет комментариев</div>
 @endif
 
 @php
-    $comments = $comments->sortBy('created_at');
-
-    if (isset($perPage)) {
-        $page = request()->query('page', 1) - 1;
-
-        $parentComments = $comments->where('child_id', '');
-
-        $slicedParentComments = $parentComments->slice($page * $perPage, $perPage);
-
-        $m = \App\Models\Comment::class; // This has to be done like this, otherwise it will complain.
-        $modelKeyName = (new $m)->getKeyName(); // This defaults to 'id' if not changed.
-
-        $slicedParentCommentsIds = $slicedParentComments->pluck($modelKeyName)->toArray();
-
-        // Remove parent Comments from comments.
-        $comments = $comments->where('child_id', '!=', '');
-
-        $grouped_comments = new \Illuminate\Pagination\LengthAwarePaginator(
-            $slicedParentComments->merge($comments)->groupBy('child_id'),
-            $parentComments->count(),
-            $perPage
-        );
-
-        $grouped_comments->withPath(request()->url());
-    } else {
-        $grouped_comments = $comments->groupBy('child_id');
-    }
+    $grouped_comments = $comments->sortBy('created_at')->groupBy('child_id');
 @endphp
 
 <x-stream target="comments">
@@ -56,5 +30,4 @@
 
 
     @include('particles.comments.form', ['model' => $model])
-
 </x-stream>
