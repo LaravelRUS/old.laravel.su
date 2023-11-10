686eb0f39c5ed3dc83555115e58a5452aac229ff
tabuna <bliz48rus@gmail.com>
2023-11-10 05:43:20 +0300

    Changed styles for docs

--- resources/js/app.js
+++ resources/js/app.js
@@ -45,3 +45,58 @@ document.addEventListener('turbo:load', () => {
         Prism.highlightElement(el);
     });
 });
+
+
+
+
+
+
+
+
+window.testLolo = function highlightSupportPolicyTable() {
+
+    function highlightCells(table) {
+        const currentDate = new Date().valueOf();
+
+        Array.from(table.rows).forEach((row, rowIndex) => {
+            if (rowIndex > 0) {
+                const cells = row.cells;
+                const versionCell = cells[0];
+                const bugDateCell = getCellDate(cells[cells.length - 2]);
+                const securityDateCell = getCellDate(cells[cells.length - 1]);
+
+                if (currentDate > securityDateCell) {
+                    // End of life.
+                    versionCell.classList.add('bg-danger', 'support-policy-highlight');
+                } else if ((currentDate <= securityDateCell) && (currentDate > bugDateCell)) {
+                    // Security fixes only.
+                    versionCell.classList.add('bg-warning', 'support-policy-highlight');
+                }
+            }
+        });
+    }
+
+    const table = document.querySelector('.documentations #support-policy ~ div table:first-of-type');
+
+    if (table) {
+        highlightCells(table);
+
+        return;
+    }
+
+    // <=v9 documentation branches use the old dom structure which doesn't contain the table overflow fix. It's easier to maintain backwards compatibility than to go back and change all the <=v9 branches.
+    const oldTable = document.querySelector('.documentations #support-policy table') || document.querySelector('.documentations table:first-of-type');
+
+    if (oldTable) {
+        highlightCells(oldTable);
+    }
+
+}
+
+function getCellDate(cell) {
+    return Date.parse(cell.innerHTML.replace(/(\d+)(st|nd|rd|th)/, '$1'));
+}
+
+document.addEventListener('turbo:load', () => {
+    window.testLolo();
+})
