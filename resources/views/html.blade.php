d53f960a875ae14019a3982c177aec7d7227b102
tabuna <bliz48rus@gmail.com>
2023-12-30 05:42:35 +0300

    Change favicon

--- resources/views/html.blade.php
+++ resources/views/html.blade.php
@@ -9,6 +9,17 @@
     <meta name="viewport"
         content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, shrink-to-fit=no, user-scalable=no, viewport-fit=cover">
 
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#c22c2c">
+    <link rel="shortcut icon" href="/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#b91d47">
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="alternate" type="application/atom+xml" title="Новости" href="/rss/feed">
