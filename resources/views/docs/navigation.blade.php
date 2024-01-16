8214ad9170682d5c1e72b987c99e1c6b08ddbc7c
SadElephant <angelforevor@mail.ru>
2024-01-16 17:35:25 +0300

    Мобильное меню документации

--- resources/views/docs/navigation.blade.php
+++ resources/views/docs/navigation.blade.php
@@ -13,24 +13,34 @@
             </select>
         </div>
         <ul class="list-unstyled">
+
             @foreach ($docs->getMenu() as $item)
                 <li class="{{ $loop->last ? '' : 'mb-4' }}">
-                    <h4 class="d-flex align-items-center rounded border-0 text-body-secondary p-0 w-100 text-start">
+                    <button
+                        class="btn btn-toggle d-flex align-items-center rounded border-0 collapsed fw-medium fs-4 text-body-secondary p-0 w-100 text-start"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#{{ \Illuminate\Support\Str::slug($item['title']) }}-collapse"
+                        aria-expanded="true">
                         {{ $item['title'] }}
-                    </h4>
+                    </button>
 
-                    <ul class="list-unstyled fw-normal pb-1 small ms-2 {{ $item['title'] === 'Пакеты' ? 'row row-cols-2' : '' }}">
-                        @foreach ($item['list'] as $link)
-                            <li class="col">
-                                <a href="{{ $link['href'] }}"
-                                   class="{{ active(url($link['href']), 'active', 'link-body-emphasis') }} d-inline-flex text-decoration-none rounded p-2">
-                                    {{ $link['title'] }}
-                                </a>
-                            </li>
-                        @endforeach
-                    </ul>
+                    <div class="collapse {{ active(collect($item['list'])->map(fn($link) => $link['href']), 'show') }} mt-2"
+                         id="{{ \Illuminate\Support\Str::slug($item['title']) }}-collapse"
+                         data-bs-parent="#docs-menu">
+                        <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ms-2">
+                            @foreach ($item['list'] as $link)
+                                <li>
+                                    <a href="{{ $link['href'] }}"
+                                       class="{{ active(url($link['href']), 'active', 'link-body-emphasis') }} d-inline-flex text-decoration-none rounded p-2">
+                                        {{ $link['title'] }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </li>
             @endforeach
+
         </ul>
     </div>
 
