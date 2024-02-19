9f083cc784868b273354fd5e501eaccdf5871cf8
tabuna <bliz48rus@gmail.com>
2024-02-19 19:27:05 +0300

    WIP

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
-@extends('layout')
+@extends('html')
 @section('title', 'Страница не найдена')
 
-@section('content')
+@section('body')
     <x-header image="/img/sign.svg">
         <x-slot:sup>Страница не найдена</x-slot>
         <x-slot:title>404</x-slot>
