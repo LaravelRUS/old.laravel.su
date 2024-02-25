68954599fc081cb884aae2f9c879a9e14de42c3f
tabuna <bliz48rus@gmail.com>
2024-02-25 05:41:47 +0300

    WIP

--- resources/views/packages/index.blade.php
+++ resources/views/packages/index.blade.php
@@ -11,35 +11,6 @@
             Наша цель — помочь русскоязычному сообществу найти новые и полезные пакеты Laravel в одном месте.
         </x-slot>
 
-            {{--
-        <x-slot:content>
-            <div class="col-6 mx-auto">
-                <img src="/img/gusli.svg" class="img-fluid">
-            </div>
-            <div class="position-relative">
-
-                <!-- Svg decoration -->
-                <figure class="position-absolute top-0 end-0 d-none d-md-block me-5">
-                    <x-icon path="l.dots" class="text-primary opacity-2" height="400" width="400" />
-                </figure>
-
-                <pre class="rounded-3 position-relative overflow-hidden bg-dark p-4 text-white shadow language-php" tabindex="0"><code
-                        class="language-php">// Получаем инсайты трендов для маркетинговой кампании
-$trendInsights = $this->getTrendInsights();
-
-// Запускаем кампанию с полученными данными
-$campaignResults = $this->executeCampaign($trendInsights);
-
-// Возвращаем результаты кампании
-return response()->json([
-    'status' => 'success',
-    'campaignResults' => $campaignResults
-]);
-</code></pre>
-            </div>
-        </x-slot:content>
---}}
-
         <x-slot:actions>
             <a href="{{route('packages.create')}}" class="btn btn-primary btn-lg px-4">Предложить пакет</a>
             <a href="{{ route('ecosystem') }}"
