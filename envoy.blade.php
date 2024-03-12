553283fae918d22bb5ba606eac108607d5ea9a22
tabuna <bliz48rus@gmail.com>
2024-03-12 05:48:54 +0300

    WIP

--- envoy.blade.php
+++ envoy.blade.php
@@ -1,28 +1,21 @@
 @servers(['web' => ['root@84.38.181.107']])
 
 @task('deploy', ['on' => ['web']])
-su deployer
-cd /home/deployer/laravel.su/current
-php artisan down --refresh=15
-git pull
+    su deployer
+    cd /home/deployer/laravel.su/current
+    php artisan down --refresh=15
+    git pull
 
-php artisan cache:clear
-php artisan config:clear
+    php artisan cache:clear
+    php artisan config:clear
 
-composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev --classmap-authoritative
+    composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev --classmap-authoritative
 
-php artisan view:clear
-php artisan sqlite:optimize
-php artisan migrate --force
-php artisan optimize
-php artisan up
-php artisan view:cache
-php artisan storage:link
-@endtask
-
-
-@task('deploy-static', ['on' => ['web']])
-su deployer
-cd /home/deployer/laravel.su/current
-git pull
+    php artisan view:clear
+    php artisan sqlite:optimize
+    php artisan migrate --force
+    php artisan optimize
+    php artisan up
+    php artisan view:cache
+    php artisan storage:link
 @endtask
